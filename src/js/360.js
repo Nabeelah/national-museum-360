2a7fe4d38e257be81d822225db4659e4509607e4
andymason <andrew.mason@guardian.co.uk>
2014-02-25 13:53:26 +0000

    Added additional 360 images

--- src/js/360.js
+++ src/js/360.js
@@ -1,13 +1,7 @@
 // 560 x 740
 
 
-var width = $('.threesixty').width();
-//$('.threesixty').css('padding-bottom', width * 1.32142);
-var height = $('.threesixty').outerHeight();
-console.log(height);
 
-width = '560';
-height = '740';
 
 $(document).on('setup',function(e) {
     $(e.target).parent().next('.click2play').hide();
