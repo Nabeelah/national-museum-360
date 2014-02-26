bf992935572c367feabfbcffeecdbabc37607e97
daanlouter <daan.louter@gmail.com>
2014-02-26 16:45:41 +0000

    changed drag message

--- src/js/360.js
+++ src/js/360.js
@@ -13,6 +13,6 @@ $(document).on('setup',function(e) {
     $(e.target).parent().next('.click2play').hide();
 });
 
-var $click2Play = $('<div class="click2play">Tap / click to load 360 rotation</div>');
+var $click2Play = $('<div class="click2play">Tap/click to load 360 rotation</div>');
 
 $('.reel').after($click2Play);
