6411a5c6b0c67e6f2529a6815e2c050d145fe528
andymason <andrew.mason@guardian.co.uk>
2014-02-26 17:50:18 +0000

    Load when scroll into view. Added iframe messenger

--- src/0.1/js/360.js
+++ src/0.1/js/360.js
@@ -2,7 +2,34 @@ $(document).on('setup',function(e) {
     $(e.target).parent().next('.click2play').hide();
 });
 
+var spinElms = $('.reel');
 var $click2Play = $('<div class="click2play">Tap / click to load 360 rotation</div>');
 
-
 $('.reel').after($click2Play);
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+function checkInView() {
+    if (spinElms) {
+        spinElms.each(function(index) {
+            if (isScrolledIntoView(this)) {
+                $(this).trigger('click');
+            }
+        });
+    }
+}
+
+// Listen to scrolling
+$(window).scroll(checkInView);
+checkInView();
+
+// Set parent iframe height
+iframeMessenger.enableAutoResize();
