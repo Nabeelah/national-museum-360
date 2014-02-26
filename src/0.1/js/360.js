e30cfd0263d81fbeb0d3157e451599d66d3c65ea
daanlouter <daan.louter@gmail.com>
2014-02-26 17:56:10 +0000

    added arrows to 360 view

--- src/0.1/js/360.js
+++ src/0.1/js/360.js
@@ -2,7 +2,11 @@ $(document).on('setup',function(e) {
     $(e.target).parent().next('.click2play').hide();
 });
 
-var $click2Play = $('<div class="click2play">Tap / click to load 360 rotation</div>');
+var $arrowRight = $('<div class="arrow-right"></div>');
+var $arrowLeft = $('<div class="arrow-left"></div>');
+// var $click2Play = $('<div class="click2play">Tap / click to load 360 rotation</div>');
 
 
-$('.reel').after($click2Play);
+$('.reel').before($arrowLeft);
+
+$('.reel').after($arrowRight);
