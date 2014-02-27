2b3c790ec28818059a714b61eb6d9f290b64e463
Daan Louter <daan.louter@gmail.com>
2014-02-27 17:12:48 +0000

    changed width of the text elements, removed commented out javascript variables

--- src/0.1/js/360.js
+++ src/0.1/js/360.js
@@ -3,17 +3,18 @@ $(document).on('setup',function(e) {
 });
 
 
-var $arrowRight = $('<div class="arrow-right arrow"></div>');
-var $arrowLeft = $('<div class="arrow-left arrow"></div>');
-$('.reel').before($arrowLeft);
-$('.reel').after($arrowRight);
-// var $click2Play = $('<div class="click2play">Tap / click to load 360 rotation</div>');
-// $('.reel').after($click2Play);
+
+
 
 var spinElms = $('.reel');
 spinElms.on('mousedown touchstart', function() {
     $(this).trigger('click');
 });
+
+var $arrowRight = $('<div class="arrow-right arrow"></div>');
+var $arrowLeft = $('<div class="arrow-left arrow"></div>');
+$('img.reel').before($arrowLeft);
+$('img.reel').after($arrowRight);
 
 
 // Set parent iframe height
