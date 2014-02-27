fe41d36a13dc435473215a0cc7dcc76a084460f3
andymason <andrew.mason@guardian.co.uk>
2014-02-27 16:59:01 +0000

    Rotate arrows next to images and loading message.

--- src/0.1/js/360.js
+++ src/0.1/js/360.js
@@ -1,14 +1,21 @@
-$(document).on('setup',function(e) {
-    $(e.target).parent().next('.click2play').hide();
-});
+// $(document).on('setup',function(e) {
+//     $(e.target).parent().next('.click2play').hide();
+// });
 
 
 var $arrowRight = $('<div class="arrow-right arrow"></div>');
 var $arrowLeft = $('<div class="arrow-left arrow"></div>');
-$('.reel').before($arrowLeft);
-$('.reel').after($arrowRight);
+// $('.reel').before($arrowLeft);
+// $('.reel').after($arrowRight);
 // var $click2Play = $('<div class="click2play">Tap / click to load 360 rotation</div>');
 // $('.reel').after($click2Play);
+
+
+
+setTimeout(function() {
+    $('.reel-overlay').prepend($arrowLeft);
+    $('.reel-overlay').append($arrowRight);
+}, 200);
 
 var spinElms = $('.reel');
 spinElms.on('mousedown touchstart', function() {
