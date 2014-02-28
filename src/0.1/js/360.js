81f8632609136263f1ab28aa7088d0e6238549e7
andymason <andrew.mason@guardian.co.uk>
2014-02-28 11:55:34 +0000

    Fixed iOS bug

--- src/0.1/js/360.js
+++ src/0.1/js/360.js
@@ -1,19 +1,24 @@
-// Build arrow DOM
-var $arrowRight = $('<div class="arrow-right arrow"></div>');
-var $arrowLeft = $('<div class="arrow-left arrow"></div>');
+
+function setupPage() {
+    // Build arrow DOM
+    var $arrowRight = $('<div class="arrow-right arrow"></div>');
+    var $arrowLeft = $('<div class="arrow-left arrow"></div>');
 
 
-// Add arrows after load
-setTimeout(function() {
-    $('.reel-overlay').prepend($arrowLeft);
-    $('.reel-overlay').append($arrowRight);
-}, 200);
+    // Add arrows after load
+    setTimeout(function() {
+        $('.reel-overlay').prepend($arrowLeft);
+        $('.reel-overlay').append($arrowRight);
+    }, 200);
 
-// Trigger click to start loading on touch/mousedown
-var spinElms = $('.reel');
-spinElms.on('mousedown touchstart', function() {
-    $(this).trigger('click');
-});
+    // Trigger click to start loading on touch/mousedown
+    var spinElms = $('.reel');
+    spinElms.on('mousedown touchstart', function() {
+        $(this).trigger('click');
+    });
+}
+
+ setupPage();
 
 
 // Set parent iframe height
